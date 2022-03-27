b2ca823c534124c21e63d6f727837d105dff2775
Costa Huang <costa.huang@outlook.com>
2022-03-27 09:38:44 -0400

    Update using mujoco bindings

--- mujoco.sh
+++ mujoco.sh
@@ -1,56 +1,39 @@
 # HalfCheetah-v4
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id HalfCheetah-v4 \
-    --cuda False \
-    --track \
-    --seed 1
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 4 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 5 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 6 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 7 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 8 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 9 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id HalfCheetah-v4 --track --seed 10 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
 
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id HalfCheetah-v4 \
-    --cuda False \
-    --track \
-    --seed 2
-
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id HalfCheetah-v4 \
-    --cuda False \
-    --track \
-    --seed 3
 
 # Walker2d-v4
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Walker2d-v4 \
-    --cuda False \
-    --track \
-    --seed 1
-
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Walker2d-v4 \
-    --cuda False \
-    --track \
-    --seed 2
-
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Walker2d-v4 \
-    --cuda False \
-    --track \
-    --seed 3
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 4 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 5 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 6 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 7 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 8 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 9 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id  Walker2d-v4 --track --seed 10 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
 
 # Hopper-v4
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Hopper-v4 \
-    --cuda False \
-    --track \
-    --seed 1
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 1 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 2 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 3 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 4 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 5 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 6 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 7 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 8 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 9 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
+OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py --cuda False --env-id Hopper-v4 --track --seed 10 --wandb-project-name cleanrl --wandb-entity openrlbenchmark
 
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Hopper-v4 \
-    --cuda False \
-    --track \
-    --seed 2
 
-OMP_NUM_THREADS=1 poetry run python ppo_continuous_action.py \
-    --env-id Hopper-v4 \
-    --cuda False \
-    --track \
-    --seed 3
+#
